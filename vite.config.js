40040acd3f73162577823701e2284db331d0f40f
hazaldoster <hazaldoster96@gmail.com>
2025-02-23 20:41:26 +0300

    updated vite config to allow railway.com domain

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
   server: {
     host: true,
     port: process.env.PORT || 3000
+  },
+  preview: {
+    host: true,
+    port: process.env.PORT || 3000,
+    allowedHosts: [
+      'beautybot-production-6bc9.up.railway.app',
+      '*.up.railway.app',
+    ]
   }
 })
